f9750b5b06fbde8c4d2d6650a8b3b5e53c9a81a8
kaisergeX <khaivd98@gmail.com>
2024-08-29 11:08:14 +0700

    fix: eslint config and lint warnings

--- apps/playground/src/hooks/react-signal.ts
+++ apps/playground/src/hooks/react-signal.ts
@@ -72,6 +72,7 @@ export const useComputed = <T>(factory: () => T): Signal<T> => {
         forceUpdate();
         return factory();
       }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   );
 };
